88f1bc6c853b4ee0ac86481f94c3c21767ed0f4e
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-01-01 14:58:35 +0100

    Add more rounds

--- Assets/Script/Round/RoundManager.cs
+++ Assets/Script/Round/RoundManager.cs
@@ -20,6 +20,27 @@
                 new BloonGroup(5.5f, BloonType.Blue, 5, 2.5f),
                 new BloonGroup(8.2f, BloonType.Red, 15, 6f)
             }),
+            
+            new Round(new[] // 4
+            {
+                new BloonGroup(0, BloonType.Red, 25, 12f),
+                new BloonGroup(7.9f, BloonType.Blue, 18, 2.5f),
+                new BloonGroup(14.5f, BloonType.Red, 10, 2.8f)
+            }),
+            
+            new Round(new[] // 5
+            {
+                new BloonGroup(0, BloonType.Blue, 12, 5.1f),
+                new BloonGroup(5.7f, BloonType.Red, 5, 2.5f),
+                new BloonGroup(8.6f, BloonType.Blue, 15, 8f)
+            }),
+            
+            new Round(new[] // 6
+            {
+                new BloonGroup(0, BloonType.Green, 4, 1.7f),
+                new BloonGroup(5.3f, BloonType.Red, 15, 5f),
+                new BloonGroup(10.2f, BloonType.Blue, 15, 8f)
+            }),
         };
     }
 }
